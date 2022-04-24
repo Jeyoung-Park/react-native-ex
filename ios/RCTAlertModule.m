4fbca89a6871390f27850036c5f2b75942dc37d5
Jeyoung-Park <jason9380@naver.com>
2022-04-25 01:46:50 +0900

    feat: ios alert 모듈 작성

---
+++ ios/RCTAlertModule.m
@@ -0,0 +1,10 @@
+#import "RCTAlertModule.h"
+
+@implementation RCTAlertModule
+
+// 네이티브 모듈을 내보내는 함수
+// 파라미터에 들어가는 이름이 네이티브 모듈 이름이 됨
+// 만약 파라미터가 비어있다면 클래스 이름에서 RCT가 제외된 이름이 모듈 이름이 됨
+RCT_EXPORT_MODULE();
+
+@end

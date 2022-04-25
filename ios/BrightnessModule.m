d9ed23d64368c97d3821a97b443f4863a0f831b0
Jeyoung-Park <jason9380@naver.com>
2022-04-26 00:25:39 +0900

    fix: 상수 내보내기

--- ios/BrightnessModule.m
+++ ios/BrightnessModule.m
@@ -2,4 +2,9 @@
 
 // 모듈 등록 -> BrightnessModule란 이름으로 모듈을 등록
 @interface RCT_EXTERN_MODULE(BrightnessModule, NSObject)
+RCT_EXTERN_METHOD(
+  getBrightness : (RCTPromiseResolveBlock)resolve
+rejecter:(RCTPromiseResolveBlock)reject
+)
+  RCT_EXTERN_METHOD(setBrightness: (CGFloat)brightness)
 @end

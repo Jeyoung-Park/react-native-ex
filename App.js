30eb9030b73bf1a125f583724c6ea4b90d683337
Jeyoung-Park <jason9380@naver.com>
2022-04-25 23:16:05 +0900

    feat: js에 적용

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {Button, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, SafeAreaView, Text} from 'react-native';
 import AlertModule from './Alert';
+import {getBrightness, setBrightness} from './Brightness';
 import ToastModule from './Toast';
 
 const App = () => {
+  const [value, setValue] = useState(0);
+  const onPress = async () => {
+    const brightness = await getBrightness();
+    console.log('brightness, ', brightness);
+    setValue(brightness);
+  };
+
+  const onPressLow = () => {
+    setBrightness(0.25);
+  };
+
+  const onPressHigh = () => {
+    setBrightness(1);
+  };
+
   return (
     <SafeAreaView
       style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Button
-        title="토스트 생성"
-        onPress={() => {
-          ToastModule.show('Hello World!', ToastModule.SHORT);
-        }}
-      />
-      <Button
-        title="Alert 생성"
-        onPress={() => {
-          AlertModule.alert('Hello World!');
-        }}
-      />
+      <Text>밝기:{value}</Text>
+      <Button title="값 갱신" onPress={onPress} />
+      <Button title=".25" onPress={onPressLow} />
+      <Button title="1" onPress={onPressHigh} />
     </SafeAreaView>
   );
 };
---
+++ Brightness.js
@@ -0,0 +1,9 @@
+import {NativeModules} from 'react-native';
+
+const {BrightnessModule} = NativeModules;
+
+// 네이티브 모듈에 어떤 메서드들이 있는지 확인하기 위함
+export const getBrightness = () => BrightnessModule.getBrightness();
+export const setBrightness = () => brightness => {
+  BrightnessModule.setBrightness(brightness);
+};

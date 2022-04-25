27933cb891cc31bd89bb4d8372958ace465c5809
Jeyoung-Park <jason9380@naver.com>
2022-04-26 01:02:16 +0900

    fix: swift 값 갱신 시 crash나는 버그 수정

--- ios/BrightnessModule.m
+++ ios/BrightnessModule.m
@@ -1,10 +1,10 @@
 #import "React/RCTBridgeModule.h"
 
 // 모듈 등록 -> BrightnessModule란 이름으로 모듈을 등록
-@interface RCT_EXTERN_MODULE(BrightnessModule, NSObject)
+@interface RCT_EXTERN_REMAP_MODULE(BrightnessModule, BrightnessModule, NSObject)
 RCT_EXTERN_METHOD(
-  getBrightness : (RCTPromiseResolveBlock)resolve
-rejecter:(RCTPromiseResolveBlock)reject
+  getBrightness: (RCTPromiseResolveBlock)resolve
+  rejecter: (RCTPromiseRejectBlock)reject
 )
   RCT_EXTERN_METHOD(setBrightness: (CGFloat)brightness)
 @end
--- ios/BrightnessModule.swift
+++ ios/BrightnessModule.swift
@@ -3,9 +3,18 @@ import UIKit
 
 @objc(BrightnessModule)
 class BrightnessModule: NSObject{
+  //  상수 export
+    @objc
+    func constantsToExport() -> [AnyHashable : Any]! {
+      return [
+        "STRING_VALUE": "Hello World",
+        "NUMBER_VALUE": 15
+      ]
+    }
+  
 //  Promise로 반환
   @objc
-  func getBrightness(_ resolve: RCTPromiseResolveBlock, rejector reject: RCTPromiseRejectBlock){
+  func getBrightness(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
     resolve(UIScreen.main.brightness)
   }
   
@@ -15,15 +24,6 @@ class BrightnessModule: NSObject{
     DispatchQueue.main.async {
       UIScreen.main.brightness=brightness
     }
-  }
-  
-//  상수 export
-  @objc
-  func constantsToExport() -> [AnyHashable : Any]! {
-    return [
-      "STRING_VALUE": "Hello World",
-      "NUMBER_VALUE": 15
-    ]
   }
   
   @objc

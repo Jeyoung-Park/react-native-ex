5b8845ed9dd63087b766f9d65e51f733b317d38b
Jeyoung-Park <jason9380@naver.com>
2022-08-18 21:30:48 +0900

    fix: post에 user data 들어가지 않는 버그 수정

--- articles-server/src/api/article/controllers/article.js
+++ articles-server/src/api/article/controllers/article.js
@@ -3,7 +3,11 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::article.article", ({ strapi }) => ({
   async create(ctx) {
     // 사용자의 id를 데이터에 추가
-    ctx.request.body.user = ctx.state.user.id;
+    ctx.request.body.data = {
+      ...ctx.request.body.data,
+      user: { id: ctx.state.user.id },
+    };
+    console.log("ctx, ", ctx.request.body);
     // article 데이터 생성
     const response = await super.create(ctx);
     // 잘못된 필드 및 private 값 제외하고 반환
@@ -11,16 +15,22 @@ module.exports = createCoreController("api::article.article", ({ strapi }) => ({
   },
 
   async update(ctx) {
-    console.log("ctx, ", ctx.req);
-    const { id } = ctx.params;
-    const article = await super.findOne({ id });
+    const userId = ctx.params?.id;
+
+    console.log("userId, ", userId);
+    const article = await strapi.entityService.findOne(
+      "api::article.article",
+      userId
+    );
 
     // article이 존재하지 않을 때 404
     if (!article) {
       return ctx.throw(404);
     }
 
-    //  정보는 변경 불가
+    console.log("ctx.state.user.id, ", ctx.state.user.id);
+
+    // 정보는 변경 불가
     if (ctx.request.body.user) {
       return ctx.throw(400, "user field cannot be changed");
     }
@@ -31,14 +41,14 @@ module.exports = createCoreController("api::article.article", ({ strapi }) => ({
     }
 
     // article update
-    const response = await super.update({ id }, ctx);
+    const response = await super.update(ctx);
 
     // 응답 반환
     return response;
   },
 
   async delete(ctx) {
-    const { id } = ctx.params;
+    const id = ctx.params?.id;
     const article = await super.findOne({ id });
 
     if (!article) {

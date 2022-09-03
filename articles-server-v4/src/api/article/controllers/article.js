8f1c251316aeb7acb8cc71a15e6d8fd1e84f9e85
Jeyoung-Park <jason9380@naver.com>
2022-09-03 22:36:50 +0900

    fix: 로직 주석 처리

--- articles-server-v4/src/api/article/controllers/article.js
+++ articles-server-v4/src/api/article/controllers/article.js
@@ -5,41 +5,39 @@ module.exports = createCoreController("api::article.article", ({ strapi }) => ({
     // 사용자의 id를 데이터에 추가
     ctx.request.body.data = {
       ...ctx.request.body.data,
-      users: ctx.state.user.id,
+      user: ctx.state.user.id,
     };
-    console.log("body, ", ctx.request.body);
     // article 데이터 생성
     const response = await super.create(ctx);
-    console.log("res, ", response);
     // 잘못된 필드 및 private 값 제외하고 반환
     return response;
   },
 
   async update(ctx) {
-    const userId = ctx.params?.id;
+    // const userId = ctx.params?.id;
 
-    console.log("userId, ", userId);
-    const article = await strapi.entityService.findOne(
-      "api::article.article",
-      userId
-    );
+    // console.log("userId, ", userId);
+    // const article = await strapi.entityService.findOne(
+    //   "api::article.article",
+    //   userId
+    // );
 
-    // article이 존재하지 않을 때 404
-    if (!article) {
-      return ctx.throw(404);
-    }
+    // // article이 존재하지 않을 때 404
+    // if (!article) {
+    //   return ctx.throw(404);
+    // }
 
-    console.log("ctx.state.user.id, ", ctx.state.user.id);
+    // console.log("ctx.state.user.id, ", ctx.state.user.id);
 
-    // 정보는 변경 불가
-    if (ctx.request.body.user) {
-      return ctx.throw(400, "user field cannot be changed");
-    }
+    // // 정보는 변경 불가
+    // if (ctx.request.body.user) {
+    //   return ctx.throw(400, "user field cannot be changed");
+    // }
 
-    // 사용자의 id와 article의 작성자 id가 일치하는지 확인
-    if (ctx.state.user.id !== article.user.id) {
-      return ctx.unauthorized("You cannot update this entry");
-    }
+    // // 사용자의 id와 article의 작성자 id가 일치하는지 확인
+    // if (ctx.state.user.id !== article.user.id) {
+    //   return ctx.unauthorized("You cannot update this entry");
+    // }
 
     // article update
     const response = await super.update(ctx);
@@ -49,31 +47,22 @@ module.exports = createCoreController("api::article.article", ({ strapi }) => ({
   },
 
   async delete(ctx) {
-    const id = ctx.params?.id;
-    const article = await super.findOne({ id });
+    // const id = ctx.params?.id;
+    // const article = await super.findOne({ id });
 
-    if (!article) {
-      return ctx.throw(404);
-    }
+    // if (!article) {
+    //   return ctx.throw(404);
+    // }
 
-    if (ctx.state.user.id !== article.user.id) {
-      return ctx.unauthorized("You cannot remove this entry");
-    }
+    // if (ctx.state.user.id !== article.user.id) {
+    //   return ctx.unauthorized("You cannot remove this entry");
+    // }
+
+    const response = await super.delete(ctx);
 
     //  응답 반환
     ctx.status = 204;
-  },
 
-  async find(ctx) {
-    // some custom logic here
-    ctx.query = { ...ctx.query, local: "en" };
-
-    // Calling the default core action
-    const { data, meta } = await super.find(ctx);
-
-    // some more custom logic
-    meta.date = Date.now();
-
-    return { data, meta };
+    return response;
   },
 }));
